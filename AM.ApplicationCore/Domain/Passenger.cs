547ba03f846c4adee81542bd9af319a1bf5500fe
EyaTizaoui <eya.tizaoui@esprit.tn>
2023-03-02 20:06:17 +0100

    Rendu

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public int passengerId { get; set; }
+        [DisplayName("Date of Birth"),DataType(DataType.Date)]
+        public DateTime birthDate { get; set; }
+        [MinLength(3,ErrorMessage ="doit etre 3 caractére"), MaxLength(25,ErrorMessage ="longeur maximal 25")]
+        
+      
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        [DataType(DataType.EmailAddress)] 
+        public string emailAdress { get; set; }
+       [RegularExpression("[0-9]{8}")]
+        public string telNumber { get; set; }
+        [Key]
+        [MaxLength(7)]
+        public string passportNumber { get; set; }
+        public List<Flight> flights { get; set; }
+
+        public override string ToString()
+        {
+            return birthDate + " " + firstName + " " + lastName + " " +  emailAdress + " " + telNumber + " " + passportNumber + " " + flights;
+        }
+
+        /*
+        public bool CheckProfile(string fName, string lName)
+        {
+            return fName == firstName && lName == lastName;
+        }
+        public bool CheckProfile(string fName, string lName, string email)
+        {
+            return fName == firstName && lName == lastName && email == emailAdress;
+        }
+        */
+
+        public bool CheckProfile(string fName, string lName, string email=null)
+        {
+            if (email==null)
+                return fName == firstName && lName == lastName;
+
+            return fName == firstName && lName == lastName && email == emailAdress;
+        }
+
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I am passenger");
+        }
+
+    }
+}
---
+++ AM.Infrastructure/Configurations/FlightConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure.Configurations
+{
+    internal class FlightConfiguration : IEntityTypeConfiguration<Flight>
+    {
+        public void Configure(EntityTypeBuilder<Flight> builder)
+        {
+            builder.HasKey(f => f.flightId);
+            builder.ToTable("MyFlight");
+            builder.Property(j => j.departure).IsRequired().HasMaxLength(100).HasColumnName("ville de depart").HasDefaultValue("Tunis").HasColumnType("nchar");
+            builder.HasOne(f => f.plane).WithMany(p => p.flights).HasForeignKey(f => f.PlaneFK).OnDelete(DeleteBehavior.SetNull);
+            builder.HasMany(f => f.passengers).WithMany(f => f.flights).UsingEntity(p => p.ToTable("My Resevation"));
+        }
+    }
+}
